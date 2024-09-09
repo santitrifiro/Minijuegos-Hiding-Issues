d2944a17c07c2a53364755fe3247e1189bf664d7
Jesus Pedalino <pedalinojesus@gmail.com>
2024-09-09 16:34:58 -0300

    Fusion con lo de Varisco

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem.Utilities;
+
 
 public class GameManager : MonoBehaviour
 {
