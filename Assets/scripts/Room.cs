b39bd998f229bdfb087ae6e3dce20d7a2a13c9be
eayazzi <galoberlini@gmail.com>
2024-09-13 11:36:11 -0300

    Fiorela2git

--- Assets/scripts/Room.cs
+++ Assets/scripts/Room.cs
@@ -1,17 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
 public class Room : MonoBehaviour
 {
 
     public int SecurityPercentage;
     public BoxCollider box;
-
-    private bool isPercentageRevealed = false;
-    private bool __reveal_next_iteration = false;
 
     // Start is called before the first frame update
     void Start()
@@ -40,40 +35,5 @@ public class Room : MonoBehaviour
         {
             player.ExitedRoom(this);
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (__reveal_next_iteration)
-        {
-            Player player = other.GetComponentInParent<Player>();
-            if (player != null)
-            {
-                player.UpdateRoom(this);
-                __reveal_next_iteration = false;
-            }
-        }
-    }
-
-    public void RevealSecurityPercentage()
-    {
-        isPercentageRevealed = true;
-        __reveal_next_iteration = true;
-    }
-
-    public int? GetSecurityPercentage()
-    {
-        if (isPercentageRevealed)
-        {
-            return SecurityPercentage;
-        } else
-        {
-            return null;
-        }
-    }
-
-    public bool IsSecurityPercentageRevealed()
-    {
-        return isPercentageRevealed;
     }
 }
--- Assets/scripts/SecurityPercentageUI.cs
+++ Assets/scripts/SecurityPercentageUI.cs
@@ -2,30 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(RectTransform))]
 public class SecurityPercentageUI : MonoBehaviour
 {
-    public string UnknownPercentageText = "???%";
-    private TextMeshProUGUI text;
+
+    public TextMeshProUGUI text;    
     // Start is called before the first frame update
     void Start()
     {
-        if (!this.TryGetComponent<RectTransform>(out var transform))
-        {
-            throw new Exception("SecurityPercentageUI: Object must have a RectTransform component");
-        }
-
-        text = this.AddComponent<TextMeshProUGUI>();
         text.text = "";
-
-        transform.anchorMin = Vector2.one;
-        transform.anchorMax = Vector2.one;
-        transform.pivot = new Vector2(1, 1);
     }
 
     public void SetSecurityPercentage(int newPercentage)
@@ -40,7 +26,7 @@ public class SecurityPercentageUI : MonoBehaviour
 
     public void UnknownSecurityPercentage()
     {
-        text.text = UnknownPercentageText;
+        text.text = "???%";
     }
 }
 
